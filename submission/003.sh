6ac85724d9ddc4018039cd0f34b7d8b0cd3ac02b
Diego Reis <reisdiego144@gmail.com>
2024-11-22 17:43:40 -0300

    solves all challenges

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,8 @@
 # How many new outputs were created by block 123,456?
+
+hash=$(bitcoin-cli getblockhash 123456)
+
+block=$(bitcoin-cli getblock  $hash 2)
+
+echo  $block | jq '[.tx[] | .vout | length] | add' 
+
--- submission/005.sh
+++ submission/005.sh
@@ -1,2 +1,18 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
+
+raw=$(bitcoin-cli getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517)
+
+decoded=$(bitcoin-cli decoderawtransaction "$raw")
+
+pubkey1=$(echo "$decoded" | jq -r '.vin[0].txinwitness[1]')
+pubkey2=$(echo "$decoded" | jq -r '.vin[1].txinwitness[1]') 
+pubkey3=$(echo "$decoded" | jq -r '.vin[2].txinwitness[1]')
+pubkey4=$(echo "$decoded" | jq -r '.vin[3].txinwitness[1]') 
+
+descriptor="sh(multi(1,$pubkey1,$pubkey2,$pubkey3,$pubkey4))"
+
+descriptor=$(bitcoin-cli -rpcwallet="SchwKatzeWL" getdescriptorinfo "$descriptor" | jq -r '.descriptor')
+
+bitcoin-cli -rpcwallet="SchwKatzeWL" deriveaddresses $descriptor | jq -r '.[0]'
+
